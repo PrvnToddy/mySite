beec2ed141a1ca8eeb47833f13e4aad5d835679c
Praveen Kumar <praveenprakasham@gmail.com>
2019-11-09 19:13:46 +0530

    Set-Cookie

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/views"));
 
 app.get("/", (req, res) => {
+  res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
